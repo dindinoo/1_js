ce4c9c5c4ba7f733236b00af9e932b8f5ab8a246
Dina Shamsitdinova <dinashamsitdinova2@gmail.com>
2024-10-03 01:07:11 +0500

    1

---
+++ 1.js
@@ -0,0 +1,11 @@
+for (let i = 13; i <= 1113; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("ThreeFive");
+    } else if (i % 3 === 0) {
+        console.log("Three");
+    } else if (i % 5 === 0) {
+        console.log("Five");
+    } else {
+        console.log(i - "не делиться на 5 и 3");
+    }
+}
